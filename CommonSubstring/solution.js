40438cbcff675492d04c56f86242e8bab87076b7
altayozyavas <altayozyavas@gmail.com>
2022-05-13 13:00:37 -0500

    fixed commonSunstring task

--- CommonSubstring/solution.js
+++ CommonSubstring/solution.js
@@ -1,16 +1,13 @@
 // Your solution here
 
-// Given two strings, determine if they share a common
-// substring. A substring may be as small as one character.
-
-// Ex:
-// hello
-// world
-// YES
-// hi
-// world
-// NO
-
 function commonSubstring(str1, str2) {
-    
+    let result = "NO";
+    const str1Chars = str1.split("");
+    const str2Chars = str2.split("");
+    str1Chars.forEach(char => {
+        if (str2Chars.includes(char)) result = "YES";
+    });
+    return result;
 }
+
+console.log(commonSubstring("Hello", "World"));
